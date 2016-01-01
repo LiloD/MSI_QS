ab0824743ea6b0faa8947724fc7a01a7713ec862
LiloD <zhizhuoding@gmail.com>
2015-12-31 22:26:29 -0500

    change question list's style, append tags after questions

--- public/modules/question/filter.js
+++ public/modules/question/filter.js
@@ -4,11 +4,31 @@
 	angular.module('CustomFilter', [])
 		.filter('cutQuestion', function(){
 			return function(input){
-				if(input.length > 120){
-					input = input.slice(0, 117)+'...';
+				if(input.length > 80){
+					input = input.slice(0, 77)+'...';
 				}
 
 				return input;
 			}
-		});
+		})
+		.filter('cutTags', function(){
+			return function(tags){
+				//120 - 80 = 40
+				//20
+				var res = [];
+				var LENGTH = 20;
+				var l = 0;
+				for(var i = 0; i < tags.length; ++i){
+					if(l + tags[i].length < LENGTH){
+						res.push(tags[i]);
+						l += tags[i].length;
+					}else{
+						res.push('...');
+						break;
+					}
+				}
+
+				return res;
+			}
+		})
 })()
